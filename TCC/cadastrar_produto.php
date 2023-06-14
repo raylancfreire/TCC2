10d4b4bf0352133d8ed9cd123bf4971cdb637f18
ChxrlesEduardo <102256915+ChxrlesEduardo@users.noreply.github.com>
2023-06-13 22:24:47 -0300

    Update cadastrar_produto.php

--- TCC/cadastrar_produto.php
+++ TCC/cadastrar_produto.php
@@ -1,21 +1,54 @@
+</html>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
+  <link rel="stylesheet" href="catalogo.css">
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <form action="CRUD/cad_produto.php" method="post" enctype="multipart/form-data">
-        <input type="text" name="nome_produto" placeholder="Nome do Produto" required><br>
-        <input type="text" name="descricao" placeholder="Descrição" required><br>
-        <input type="text" name="marca" placeholder="Marca" required><br>
-        <input type="text" name="categoria" placeholder="Categoria" required><br>
-        <input type="number" name="preco" placeholder="Preço" step="0.01" required><br>
-        <input type="number" name="quantidade_produto" placeholder="Quantidade do Produto" required><br>
-        <input type="file" name="imagem" required><br>
-        <button type="submit">Enviar</button>
-    </form>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                    <form method="GET" action="catalogo.php">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="nome_produto" placeholder="Digite o nome do produto">
+                        <button class="btn btn-primary" type="submit">Buscar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+        </div>
+    </div>
 </body>
 </html>
+<?php
+    // Verifica se o formulário de busca foi enviado
+    if (isset($_GET['nome_produto'])) {
+        $nomeProduto = $_GET['nome_produto'];
+
+        // Consulta os dados da tabela "produtos" com base no nome do produto
+        $sql = "SELECT path, nome_produto, descricao, preco FROM produtos WHERE nome_produto LIKE '%$nomeProduto%'";
+        $result = $pdo->query($sql);
+
+        // Exibe os produtos em forma de catálogo
+        if ($result->rowCount() > 0) {
+            foreach ($result as $row) {
+                // Exibe os detalhes do produto
+                echo "<div class='product'>";
+                echo "<img class='product-image' src='upload/{$row['path']}' alt='Imagem do produto'>";
+                echo "<h3 class='product-name'>{$row['nome_produto']}</h3>";
+                echo "<p class='product-description'>{$row['descricao']}</p>";
+                echo "<p class='product-price'>Preço: R$ {$row['preco']}</p>";
+                echo "<button class='botao-comprar'>Comprar</button>";
+                echo "</div>";
+            }
+        } else {
+            echo "<p>Nenhum produto encontrado.</p>";
+        }
+    }
+?>
+
