70eaf2d62d299d9de482b7b415b3d0893351a316
PEDRO HENRIQUE COELHO PEDRINI <ppedrini@labcetec.net>
2023-06-14 15:15:54 -0300

    Fiz algumas separacoes de arquivos, separando o html do php

--- TCC/catalogo_luan.php
+++ TCC/catalogo_luan.php
@@ -6,7 +6,7 @@
     ?>
 <html lang="pt-br">
 <head>
-  <link rel="stylesheet" href="catalogo.css">
+  <link rel="stylesheet" href="CSS/catalogo.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Index</title>
