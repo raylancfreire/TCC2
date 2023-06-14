e6b5f45a05003946332fe42d1e2cf0b3018ac734
raylancfreire <102255246+raylancfreire@users.noreply.github.com>
2023-06-14 01:57:25 -0300

    Adicionando o Projeto atualizado
    JUNTEI TUDO QUE DEU PRA JUNTAR, O QUE EU CHARLES E DLUCCA FIZEMOS, ASSIM VAI SER MELHOR PRA GENTE CONTINUAR MEXENDO NO PROJETO

---
+++ TCC/carrinho.php
@@ -0,0 +1,34 @@
+<?php
+    $includeNavbar = true;
+    if ($includeNavbar) {
+        include("navbar.php"); // Inclui a navbar
+    }
+    ?>
+<?php
+require("conn.php");
+
+// Obtém o ID atual
+$id_atual = $_GET['comprar'];
+
+// Consulta SQL para selecionar o produto com o ID atual
+$sql = "SELECT * FROM produtos WHERE id_produto = :id";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['id' => $id_atual]);
+$produto = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// Verifica se o produto foi encontrado
+if ($produto) {
+    echo "<h2>{$produto['nome_produto']}</h2>";
+    echo "<p>{$produto['descricao']}</p>";
+    echo "<p>Marca: {$produto['marca']}</p>";
+    echo "<p>Categoria: {$produto['categoria']}</p>";
+    echo "<p>Preço: R$ {$produto['preco']}</p>";
+    echo "<p>Quantidade: {$produto['quantidade_produto']}</p>";
+    // Exibir a imagem aqui
+    echo "<img src='upload/{$produto['path']}' alt='Imagem do produto'>";
+} else {
+    echo "Produto não encontrado.";
+}
+
+// Lembre-se de tratar exceções e fechar a conexão com o banco de dados quando terminar.
+?>
---
+++ TCC/catalogo_luan.php
@@ -0,0 +1,71 @@
+<?php
+    $includeNavbar = true;
+    if ($includeNavbar) {
+        include("navbar.php"); // Inclui a navbar
+    }
+    ?>
+<html lang="pt-br">
+<head>
+  <link rel="stylesheet" href="catalogo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                    <form method="GET" action="catalogo.php">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="nome_produto" placeholder="Digite o nome do produto">
+                        <button class="btn btn-primary" type="submit">Buscar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<div class="container">
+    <div class="row">
+      <div class="col mt-5">
+        <!-- Conteúdo do catálogo -->
+        <div class="catalog">
+          <h1>Catálogo</h1>
+              <?php
+              require("conn.php");
+
+              // Consulta os dados da tabela "produtos"
+              $sql = "SELECT path, nome_produto, descricao, preco FROM produtos";
+              $result = $pdo->query($sql);
+
+              // Exibe os produtos em forma de catálogo
+              if ($result->rowCount() > 0) {
+                echo "<div class='catalog'>"; // Início do catálogo
+
+                foreach ($result as $row) {
+                  // Exibe os detalhes do produto
+                  echo "<div class='product'>";
+                  echo "<img class='product-image' src='upload/{$row['path']}' alt='Imagem do produto'>";
+                  echo "<h3 class='product-name'>{$row['nome_produto']}</h3>";
+                  echo "<p class='product-description'>{$row['descricao']}</p>";
+                  echo "<p class='product-price'>Preço: R$ {$row['preco']}</p>";
+                  echo "<button class='botao-comprar'>Comprar</button>";
+                  echo '<button><a href=tela_compra.php?produto='.['id_produto'].' class="btn btn-success">COMPRAR</a></button>';
+                  echo "</div>";
+                }
+
+                echo "</div>"; // Fim do catálogo
+              } else {
+                echo "<p>Nenhum produto encontrado.</p>";
+              }
+              ?>
+
+        </div>
+      </div>
+    </div>
+  </div>
