d3374c0ab507fe026906f5b91832e61992e925d7
ChxrlesEduardo <102256915+ChxrlesEduardo@users.noreply.github.com>
2023-06-14 17:19:58 -0300

    Update logar.php

--- tcc/logar.php
+++ tcc/logar.php
@@ -4,47 +4,53 @@
     $senha = $_POST['senha'];
 
     $usuarios = $pdo->prepare('SELECT * FROM usuarios where email=:email
-    AND senha_hash=:senha');
-    $usuarios->execute(array(':email'=>$email,':senha_hash'=>$senha));
+    AND senha=:senha');
+    $usuarios->execute(array(':email'=>$email,':senha'=>$senha));
 
     $rowTabela = $usuarios->fetchAll();
     
     if (empty($rowTabela)){
         echo "<script>
-        Swal.fire({
-            icon: 'error',
-            title: 'Erro!',
-            text: 'Usuário e/ou senha inválidos!!!',
-            confirmButtonText: 'OK',
-            onClose: function() {
-                history.back();
-            }
-        }).then(function() {
-            history.back();
-        });
+        alert('Usuario e/ou senha invalidos!!!');
+        window.location.href='login.php';
         </script>";
-    }else {
+    } else {
         $sessao = $rowTabela[0];
-    
-        if(!isset($_SESSION)) {
-            session_start();
-        }
-        $_SESSION['id_login'] = $sessao['id_login'];
-        $_SESSION['login'] = $sessao['login'];
-    
-        echo "<script>
-        Swal.fire({
-            icon: 'success',
-            title: 'Bem-vindo!',
-            text: 'Login bem-sucedido! Bem-vindo, " . $_SESSION['login'] . "!',
-            confirmButtonText: 'OK',
-            onClose: function() {
-                window.location.href = 'index.php';
-            }
-        }).then(function() {
-            window.location.href = 'index.php';
-        });
-        </script>";
-        }
+        
 
+        if(!isset($_SESSION)) {
+        session_start();
+        }
+        $_SESSION['id_usuario'] = $sessao['id_usuario'];
+        $_SESSION['email'] = $sessao['email'];
+        $_SESSION['endereco'] = $sessao['endereco'];
+
+        
+    
+        header("Location: index.php");
+        }
+    
+        $empresas = $pdo->prepare('SELECT * FROM empresas where email=:email
+        AND senha=:senha');
+        $empresas->execute(array(':email'=>$email,':senha'=>$senha));
+    
+        $rowTabela = $empresas->fetchAll();
+        
+        if (empty($rowTabela)){
+            echo "<script>
+            alert('Usuario e/ou senha invalidos!!!');
+            window.location.href='login.php';
+            </script>";
+        } else {
+            $sessao = $rowTabela[0];
+    
+            if(!isset($_SESSION)) {
+            session_start();
+            }
+            $_SESSION['id_empresa'] = $sessao['id_empresa'];
+            $_SESSION['email'] = $sessao['email'];
+        
+            header("Location: tela_empresa.php");
+            }
+    
 ?>
