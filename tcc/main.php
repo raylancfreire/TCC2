baab675bb569bf048f845692b7aa43e3161f828d
ChxrlesEduardo <102256915+ChxrlesEduardo@users.noreply.github.com>
2023-06-14 23:25:11 -0300

    Update main.php

--- tcc/main.php
+++ tcc/main.php
@@ -7,33 +7,8 @@ if ($includeNavbar) {
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <style>
-        .popup {
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%) scale(1.5);
-            padding: 20px;
-            background-color: #f1f1f1;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            z-index: 9999;
-            text-align: center;
-            opacity: 3;
-            animation: fadeOut 5s forwards;
-        }
-
-        @keyframes fadeOut {
-            0% { opacity: 1; }
-            100% { opacity: 0; }
-        }
-
-        .success-popup {
-            background-color: #c9f6d6;
-            border-color: #5bc366;
-        }
-    </style>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
@@ -63,7 +38,7 @@ if ($includeNavbar) {
                     setTimeout(function() {
                         popup.remove();
                     }, 1000);
-                }, 2000);
+                }, 3000);
             }
         });
     </script>
