618bbf4feef10a8a70dd25554510c8c12993ef0a
ChxrlesEduardo <102256915+ChxrlesEduardo@users.noreply.github.com>
2023-06-14 22:36:43 -0300

    Update cad_usuario.php

--- tcc/CRUD/cad_usuario.php
+++ tcc/CRUD/cad_usuario.php
@@ -3,23 +3,23 @@ require('../conn.php');
 
 $nome_usuario = $_POST['nome_usuario'];
 $cpf = $_POST['cpf'];
+$endereco = $_POST['endereco'];
 $email = $_POST['email'];
-$senha = $_POST['senha_hash'];
+$senha = $_POST['senha'];
 
-$senha_hash = password_hash($senha, PASSWORD_DEFAULT);
-
-if (empty($nome_usuario) || empty($cpf) || empty($email) || empty($senha_hash)) {
+if (empty($nome_usuario) || empty($cpf) || empty($endereco) || empty($email) || empty($senha)) {
     echo "Os valores não podem ser vazios";
 } elseif (!preg_match('/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/', $cpf)) {
     echo "CPF inválido. Por favor, insira um CPF válido no formato xxx.xxx.xxx-xx.";
 } else {
-    $cad_usuarios = $pdo->prepare("INSERT INTO usuarios(nome_usuario, cpf, email, senha_hash) 
-        VALUES(:nome_usuario, :cpf, :email, :senha_hash)");
+    $cad_usuarios = $pdo->prepare("INSERT INTO usuarios(nome_usuario, cpf, endereco, email, senha) 
+        VALUES(:nome_usuario, :cpf, :endereco, :email, :senha)");
     $cad_usuarios->execute(array(
         ':nome_usuario' => $nome_usuario,
         ':cpf' => $cpf,
+        ':endereco' => $endereco,
         ':email' => $email,
-        ':senha_hash' => $senha_hash
+        ':senha' => $senha
     ));
 
     echo "<script>
