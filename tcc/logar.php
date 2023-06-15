e7bbe44f26b9d0a1721e0e9311ce48713653557b
ChxrlesEduardo <102256915+ChxrlesEduardo@users.noreply.github.com>
2023-06-14 22:36:06 -0300

    Update logar.php

--- tcc/logar.php
+++ tcc/logar.php
@@ -1,5 +1,6 @@
 <?php
     include("conn.php");
+    session_start();
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
@@ -9,48 +10,16 @@
 
     $rowTabela = $usuarios->fetchAll();
     
-    if (empty($rowTabela)){
-        echo "<script>
-        alert('Usuario e/ou senha invalidos!!!');
-        window.location.href='login.php';
-        </script>";
+    if (empty($rowTabela)) {
+        $_SESSION['login_status'] = 'error';
+        header("Location: login.php");
     } else {
         $sessao = $rowTabela[0];
-        
-
-        if(!isset($_SESSION)) {
-        session_start();
-        }
+        // Armazene outras informações de sessão que você deseja exibir
         $_SESSION['id_usuario'] = $sessao['id_usuario'];
         $_SESSION['email'] = $sessao['email'];
         $_SESSION['endereco'] = $sessao['endereco'];
-
-        
-    
+        $_SESSION['login_status'] = 'success';
         header("Location: index.php");
-        }
-    
-        $empresas = $pdo->prepare('SELECT * FROM empresas where email=:email
-        AND senha=:senha');
-        $empresas->execute(array(':email'=>$email,':senha'=>$senha));
-    
-        $rowTabela = $empresas->fetchAll();
-        
-        if (empty($rowTabela)){
-            echo "<script>
-            alert('Usuario e/ou senha invalidos!!!');
-            window.location.href='login.php';
-            </script>";
-        } else {
-            $sessao = $rowTabela[0];
-    
-            if(!isset($_SESSION)) {
-            session_start();
-            }
-            $_SESSION['id_empresa'] = $sessao['id_empresa'];
-            $_SESSION['email'] = $sessao['email'];
-        
-            header("Location: tela_empresa.php");
-            }
-    
+    }
 ?>
